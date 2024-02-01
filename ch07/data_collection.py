bece0f0e7a7230078d309283baa700838ff809b9
ChoiJinHyeok <chlwlsgur000@naver.com>
2024-02-01 13:23:50 +0900

    20240201

--- ch07/data_collection.py
+++ ch07/data_collection.py
@@ -9,20 +9,19 @@ load_dotenv()
 SERVICE_KEY = os.getenv('SEOUL_API_KEY')
 print(SERVICE_KEY)
 
+def main():
+    data = None
+    for j in range(1,5):
+        url = f'http://openapi.seoul.go.kr:8088/{SERVICE_KEY}/json/tbLnOpendataRtmsV/{1+((j-1)*1000)}/{j*1000}'
+        print(url)
+        req = requests.get(url)
+        content = req.json()
+        con = content['tbLnOpendataRtmsV']['row']
+        result = pd.DataFrame(con)
+        data = pd.concat([data, result])
+    data = data.reset_index(drop=True)
+    data['DEAL_YMD'] = pd.to_datetime(data['DEAL_YMD'], format=("%Y%m%d"))
+    data.to_csv('./data/seoul_real_estate.csv', index=False)
 
-
-
-
-
-
-
-# data = None
-# for j in range(1,5):
-#     url = f'http://openapi.seoul.go.kr:8088/{SERVICE_KEY}/json/tbLnOpendataRtmsV/{1+((j-1)*1000)}/{j*1000}'
-#     print(url)
-#     req = requests.get(url)
-#     content = req.json()
-#     con = content['tbLnOpendataRtmsV']['row']
-#     result = pd.DataFrame(con)
-#     data = pd.concat([data, result])
-# data = data.reset_index(drop=True)
+if __name__ == '__main__':
+    main()
